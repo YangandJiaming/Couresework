d168b6beaf5f15b4b7e79a256f1635746c6aa093
yy2916105162 <70853126+yy2916105162@users.noreply.github.com>
2021-01-08 00:02:26 +0000

    Update B2_non-linear.py

--- B2_non-linear.py
+++ B2_non-linear.py
@@ -1,8 +1,9 @@
 import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
-import sympy as sym
 
+
+# Given values
 m = 0.425
 g = 9.81
 d = 0.42
@@ -16,27 +17,37 @@ c = 6.815
 k = 1880
 b = 10.4
 phi = 42.*np.pi/180
-V = 39.7
+
+# Equilibrium points
+x1e=0.478
+Ie=(((-m*g*np.sin(phi)+k*(x1e-d))*((delta-x1e)**2)/c))**(1/2)
+Ve=Ie*R
 
 
+# Non-linear dynamic equations of the system
 def ballmotion(_t, z):
     x1 = z[0]
     x2 = z[1]
     I = z[2]
     return [x2,
-            (5/7*m)*(m*g*np.sin(phi)-k*(x1-d)-b*x2+c*((I)**2)/((delta-x1)**2)),
-            (V-I*R)/(L0+L1*np.exp(-alpha*(delta-x1)))]
+            (5/7*m)*((m*g*np.sin(phi))-k*(x1-d)-b*x2+((c*(I)**2)/((delta-x1)**2))),
+            (Ve-I*R)/(L0+L1*np.exp(-alpha*(delta-x1)))]
 
-
-z_initial = [0.4876, 0, 0.75]
-t_final = 2
+# Initial value theorem
+z_initial = [0.48, 0, Ie]
+t_final = 5
 num_points = 1000
 sol = solve_ivp(ballmotion,
                 [0, t_final],
                 z_initial,
                 t_eval=np.linspace(0, t_final, num_points))
+times = sol.t
+x_trajectory = sol.y[0]
 
-x1_simulation = sol.y[0]
-
-plt.plot(sol.t, sol.y[0].T)
+# Plot the Trajectory of ball
+plt.plot(times, x_trajectory.T)
+plt.xlabel('Time (s)')
+plt.ylabel('Ball position (m)')
+plt.title('Ball trajectory')
+plt.grid()
 plt.show()
