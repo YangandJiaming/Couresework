72f0308b7f1bfedd5e7c48fbfb0c51104aef6fa9
yy2916105162 <70853126+yy2916105162@users.noreply.github.com>
2021-01-10 11:29:52 +0000

    Update response.py

--- response.py
+++ response.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sympy as sym
-
+import control as ctrl
+import matplotlib.pyplot as plt
 
 # Given values
 m = 0.425
@@ -18,9 +19,11 @@ b = 10.4
 phi = 42.*np.pi/180
 x1e=0.478
 
+
 # Equilibrium points
 Ie=(((-m*g*np.sin(phi)+k*(x1e-d))*((delta-x1e)**2)/c))**(1/2)
 Ve=Ie*R
+
 
 # Values of Parameters
 A=(k-(2*c*(Ie)**2)/((delta-x1e)**3))*(((delta-x1e)**2)/(2*c*Ie))
@@ -33,14 +36,37 @@ P=B*D+C*R
 J=A*D+B*R
 K=A*R+E
 
+
 # Transfer function of the system
+transfer_function = ctrl.TransferFunction([1], [O,P,J,K])
+
+# determine impulse response and step response of system
 s, t = sym.symbols('s, t')
-transfer_function = 1 /(O*(s**3) + P*(s**2) + J*s + K)
+transfer_function_1 = 1/(O*(s)**3 + P*(s)**2 + J*s + K)
+F_input_impulse = 1
+F_input_step = 1/s
+impulse_response=sym.inverse_laplace_transform(transfer_function_1 * F_input_impulse, s, t)
+step_response=sym.inverse_laplace_transform(transfer_function_1 * F_input_step, s, t)
 
-# Impulse response and Step response of the system
-impulse_response=sym.inverse_laplace_transform(transfer_function * 1 , s, t)
-step_response=sym.inverse_laplace_transform(transfer_function * (1/s) , s, t)
-
+# Print the response 
 sym.pprint(impulse_response.simplify())
 sym.pprint(step_response.simplify())
+
+# Impulse response and step response of this system
+t,y = ctrl. impulse_response(transfer_function,
+                             np.linspace(0, 5, 5000))
+
+t1,y1 = ctrl. step_response(transfer_function,
+                             np.linspace(0, 5, 5000))
+
+
+# Plot the response of system
+plt.xlabel('Time (s)')
+plt.ylabel('Ball position (m)')
+plt.title('Impulse Response and Step Response of System')
+plt.plot(t, y,label='impulse response')
+plt.plot(t1, y1,label='step response')
+plt.legend(loc='upper right')
+plt.grid()
+plt.show()
 
